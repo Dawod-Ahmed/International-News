3e21ac0076accf4f2d4f3af2b84e7491bb8ca758
Dawood Ahmed <DaudAhmed27786@gmail.com>
2022-03-29 08:49:28 +0400

    Develop News Clone

---
+++ src/components/articles.js
@@ -0,0 +1,267 @@
+
+export default  aritcles = [
+    {
+      "source": {
+        "id": "engadget",
+        "name": "Engadget"
+      },
+      "author": "Kris Holt",
+      "title": "Samsung's $700 Smart Monitor M8 is now available to pre-order",
+      "description": "Samsung's latest Smart Monitor, which we first got a look at\r\n during CES, is now available to pre-order\r\n. The Smart Monitor M8 has support for streaming services including Netflix, Amazon Prime Video, Disney+ and Apple TV, as well as cloud gaming platforms\r…",
+      "url": "https://www.engadget.com/samsung-smart-monitor-m8-price-pre-order-104039969.html",
+      "urlToImage": "https://s.yimg.com/os/creatr-uploaded-images/2022-03/e1c62970-ae80-11ec-b631-f73cb3e02ca8",
+      "publishedAt": "2022-03-28T10:40:39Z",
+      "content": "Samsung's latest Smart Monitor, which we first got a look at\r\n during CES, is now available to pre-order\r\n. The Smart Monitor M8 has support for streaming services including Netflix, Amazon Prime Vid… [+1786 chars]"
+    },
+    {
+      "source": {
+        "id": "engadget",
+        "name": "Engadget"
+      },
+      "author": "Sam Rutherford",
+      "title": "OnePlus as we knew it is dead, here's what next",
+      "description": "OnePlus began life as a startup making smartphones with high-end specs aimed at enthusiasts. Its first phones were barebones devices that maximized value for money, while still retaining a sense of identity (remember those sandstone backs?). That’s something …",
+      "url": "https://www.engadget.com/one-plus-as-we-knew-it-is-dead-heres-what-next-133017093.html",
+      "urlToImage": "https://s.yimg.com/os/creatr-uploaded-images/2022-03/52f9d240-a530-11ec-befd-7511c12e9613",
+      "publishedAt": "2022-03-28T13:30:17Z",
+      "content": "OnePlus began life as a startup making smartphones with high-end specs aimed at enthusiasts. Its first phones were barebones devices that maximized value for money, while still retaining a sense of i… [+9634 chars]"
+    },
+    {
+      "source": {
+        "id": "wired",
+        "name": "Wired"
+      },
+      "author": "Angela Watercutter",
+      "title": "Apple TV+ Just Won Best Picture. Everything Is Different Now",
+      "description": "Hollywood has been waiting for years to see if a streaming service could nab the Oscars’ highest honor. This year, they got a historic answer.",
+      "url": "https://www.wired.com/story/oscars-streaming-services/",
+      "urlToImage": "https://media.wired.com/photos/623d0ec947eae4e024ce6da2/191:100/w_1280,c_limit/Oscars-Streaming-CODA-Culture-0111.jpg",
+      "publishedAt": "2022-03-28T03:52:48Z",
+      "content": "Toward the end of 2021, the buzz was palpable. The Power of the Dog, director Jane Champions Western allegory about toxic masculinity, was on track to be the first Best Picture Oscar winner for Netfl… [+4672 chars]"
+    },
+    {
+      "source": {
+        "id": "the-verge",
+        "name": "The Verge"
+      },
+      "author": "Mitchell Clark",
+      "title": "Apple is reportedly already cutting iPhone SE production plans by 20 percent",
+      "description": "Apple will reportedly make fewer iPhone SEs than it initially planned next quarter, with analysts saying it could be reducing production by 20 percent. Another analyst predicts it’ll ship far fewer of the phones this year.",
+      "url": "https://www.theverge.com/2022/3/28/23000004/apple-iphone-se-production-drop-shipping-numbers",
+      "urlToImage": "https://cdn.vox-cdn.com/thumbor/ZGXkeWJofeRLiIWNzEO7PN6oxiQ=/0x146:2040x1214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/23307882/ajohnson_221103_5070_0002.jpg",
+      "publishedAt": "2022-03-28T19:35:40Z",
+      "content": "The iPhone SE may be easy to make, but it could be hard to sell\r\nThe iPhone SE might not be meeting Apples sales expectations.\r\nPhoto by Allison Johnson / The Verge\r\nWhile luxury electronics like the… [+2961 chars]"
+    },
+    {
+      "source": {
+        "id": "the-verge",
+        "name": "The Verge"
+      },
+      "author": "Victoria Song",
+      "title": "Chipolo Card Spot review: a less creepy AirTag",
+      "description": "What you’d get if you crossed a Tile with an AirTag",
+      "url": "https://www.theverge.com/22996801/chipolo-card-spot-review-find-my",
+      "urlToImage": "https://cdn.vox-cdn.com/thumbor/W8PrRns_IIlr9QZ7Xrb76G17gxg=/0x292:2040x1360/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/23342394/vsong_220323_5100_0004.jpg",
+      "publishedAt": "2022-03-28T17:30:00Z",
+      "content": "If you buy something from a Verge link, Vox Media may earn a commission. See our ethics statement.Item trackers were made for absent-minded people like me. Not a day goes by that I dont ping my iPhon… [+9845 chars]"
+    },
+    {
+      "source": {
+        "id": null,
+        "name": "Gizmodo.com"
+      },
+      "author": "Phillip Tracy",
+      "title": "Apple Is Making Us Want to Buy the iPad Pro Again",
+      "description": "Apple refreshed the iPad Air at its “Peek Performance” event earlier this month, trading the tablet’s A-series processor for an M1 chip. With the iPad Air matching the Pro’s performance, purchasing the more expensive model seemed like a waste of money. Apple …",
+      "url": "https://gizmodo.com/ipad-pro-with-m2-chip-magsafe-arriving-this-fall-repo-1848712099",
+      "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/b6bfbfda174dad5de481715849bb674f.jpg",
+      "publishedAt": "2022-03-28T16:10:00Z",
+      "content": "Apple refreshed the iPad Air at its Peek Performance event earlier this month, trading the tablets A-series processor for an M1 chip. With the iPad Air matching the Pros performance, purchasing the m… [+1578 chars]"
+    },
+    {
+      "source": {
+        "id": null,
+        "name": "Gizmodo.com"
+      },
+      "author": "Mack DeGeurin",
+      "title": "An Angry Stalker Used an Apple Watch Wrapped Around His Ex's Tire Spokes to Secretly Track Her",
+      "description": "A 19-year-old Tennessee man was arrested over the weekend for allegedly attempting to surreptitiously track his partner using an Apple Watch hooked around the wheel of her car.Read more...",
+      "url": "https://gizmodo.com/an-angry-stalker-used-an-apple-watch-wrapped-around-his-1848714771",
+      "urlToImage": "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/caac41d32bcf7c67e7b71a62610dfc43.jpg",
+      "publishedAt": "2022-03-28T19:10:00Z",
+      "content": "A 19-year-old Tennessee man was arrested over the weekend for allegedly attempting to surreptitiously track his partner using an Apple Watch hooked around the wheel of her car.\r\nPolice reportedly lea… [+4626 chars]"
+    },
+    {
+      "source": {
+        "id": "reuters",
+        "name": "Reuters"
+      },
+      "author": null,
+      "title": "Apple to cut iPhone, AirPods output - Nikkei - Reuters.com",
+      "description": "Apple Inc <a href=\"https://www.reuters.com/companies/AAPL.O\" target=\"_blank\">(AAPL.O)</a> is planning to cut the output of its iPhone SE and AirPods devices, the Nikkei reported on Monday, citing sources.",
+      "url": "https://www.reuters.com/technology/apple-cut-iphone-airpods-output-nikkei-2022-03-28/",
+      "urlToImage": "https://www.reuters.com/resizer/jlIVK6oacxteivKVwwhsY8anmVA=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/S67UUXDA3VLNHD3WCPVWZFC5AI.jpg",
+      "publishedAt": "2022-03-28T05:24:00Z",
+      "content": "March 28 (Reuters) - Apple Inc (AAPL.O) is planning to cut the output of its iPhone SE and AirPods devices, the Nikkei reported on Monday, citing sources.\r\nThe company plans to make about 20% fewer i… [+343 chars]"
+    },
+    {
+      "source": {
+        "id": "reuters",
+        "name": "Reuters"
+      },
+      "author": "Reuters Editorial",
+      "title": "Apple to cut iPhone, AirPods output - Nikkei - Reuters.com",
+      "description": "Apple is planning to cut the output of its iPhone and AirPods devices as the Ukraine crisis and looming inflation start to weigh on demand for consumer electronics, the Nikkei reported on Monday, citing sources. Francis Maguire reports.",
+      "url": "https://www.reuters.com/video/watch/idOV785228032022RP1",
+      "urlToImage": "https://static.reuters.com/resources/r/?d=20220328&i=OV785228032022RP1&r=OV785228032022RP1&t=2",
+      "publishedAt": "2022-03-28T11:16:21Z",
+      "content": "Posted \r\nApple is planning to cut the output of its iPhone and AirPods devices as the Ukraine crisis and looming inflation start to weigh on demand for consumer electronics, the Nikkei reported on Mo… [+45 chars]"
+    },
+    {
+      "source": {
+        "id": "reuters",
+        "name": "Reuters"
+      },
+      "author": null,
+      "title": "Biden budget plan seeks to add corporate buyback restrictions - Reuters.com",
+      "description": "U.S. corporate buybacks are being targeted in U.S. President Joe Biden's 2023 budget plan announced Monday, which seeks to discourage corporations from using profits to repurchase stocks in order to benefit executives.",
+      "url": "https://www.reuters.com/world/us/biden-budget-plan-seeks-add-corporate-buyback-restrictions-2022-03-28/",
+      "urlToImage": "https://www.reuters.com/resizer/O5qTVjNJq39-gJQFyoAKkTXwcbo=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/JENENCSLBVOETEY362UNX67QX4.jpg",
+      "publishedAt": "2022-03-28T17:13:00Z",
+      "content": "NEW YORK, March 28 (Reuters) - U.S. corporate buybacks are being targeted in U.S. President Joe Biden's 2023 budget plan announced Monday, which seeks to discourage corporations from using profits to… [+1028 chars]"
+    },
+    {
+      "source": {
+        "id": "reuters",
+        "name": "Reuters"
+      },
+      "author": null,
+      "title": "Hollywood execs stay silent on Will Smith Oscar incident - Reuters.com",
+      "description": "A day after superstar Will Smith slapped comedian Chris Rock minutes before accepting the best actor Oscar at the 94th Academy Awards, Hollywood power brokers remained publicly silent even as celebrities criticized the attack on social media.",
+      "url": "https://www.reuters.com/lifestyle/hollywood-execs-stay-silent-will-smith-oscar-incident-2022-03-28/",
+      "urlToImage": "https://www.reuters.com/resizer/wElQV8A9w_5--tHuIJqYjhw-Y6A=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/7VFGU54SB5I7JPL7PWEZYXJKIA.jpg",
+      "publishedAt": "2022-03-28T23:16:00Z",
+      "content": "March 28 (Reuters) - A day after superstar Will Smith slapped comedian Chris Rock minutes before accepting the best actor Oscar at the 94th Academy Awards, Hollywood power brokers remained publicly s… [+2870 chars]"
+    },
+    {
+      "source": {
+        "id": "reuters",
+        "name": "Reuters"
+      },
+      "author": null,
+      "title": "Tesla to ask shareholders to vote on authorizing more shares - Reuters.com",
+      "description": "Tesla Inc <a href=\"https://www.reuters.com/companies/TSLA.O\" target=\"_blank\">(TSLA.O)</a> said on Monday it would ask shareholders to vote at its annual meeting to increase the number of additional shares in order to enable a stock split.",
+      "url": "https://www.reuters.com/business/autos-transportation/tesla-ask-shareholders-vote-authorizing-more-shares-2022-03-28/",
+      "urlToImage": "https://www.reuters.com/resizer/_68U9kUVFqnQIuyyvwpBtNqmWOU=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/4RO5A3GUZFODHBXYQZDA732INM.jpg",
+      "publishedAt": "2022-03-28T10:28:00Z",
+      "content": "March 28 (Reuters) - Tesla Inc (TSLA.O) said on Monday it would ask shareholders to vote at its annual meeting to increase the number of additional shares in order to enable a stock split.\r\nThe Austi… [+783 chars]"
+    },
+    {
+      "source": {
+        "id": "reuters",
+        "name": "Reuters"
+      },
+      "author": null,
+      "title": "Futures inch higher as oil eases, Tesla gains - Reuters.com",
+      "description": "U.S. stock index futures inched higher on Monday as oil prices slid and investors remained hopeful about progress in the Russia-Ukraine peace talks, while Tesla shares jumped after the company sought investor approval for a stock split.",
+      "url": "https://www.reuters.com/business/futures-inch-higher-oil-eases-tesla-gains-2022-03-28/",
+      "urlToImage": "https://www.reuters.com/resizer/dgiPgD_GXCDQZAKwK7634X8JjRk=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/NA5EATQ5OBIVXIZ3PLQDJAIWXQ.jpg",
+      "publishedAt": "2022-03-28T12:04:00Z",
+      "content": "March 28 (Reuters) - U.S. stock index futures inched higher on Monday as oil prices slid and investors remained hopeful about progress in the Russia-Ukraine peace talks, while Tesla shares jumped aft… [+2040 chars]"
+    },
+    {
+      "source": {
+        "id": "reuters",
+        "name": "Reuters"
+      },
+      "author": null,
+      "title": "Tesla adds to wave of megacap stock splits - Reuters",
+      "description": "Tesla's <a href=\"https://www.reuters.com/companies/TSLA.O\" target=\"_blank\">(TSLA.O)</a> announcement on Monday that it will seek shareholder approval to increase its share count in order to enable a stock split adds to a recent wave of megacap companies split…",
+      "url": "https://www.reuters.com/business/autos-transportation/tesla-adds-wave-megacap-stock-splits-2022-03-28/",
+      "urlToImage": "https://www.reuters.com/resizer/LXEwaf1X8dH35zJ7wS9fzLaQNt8=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/A6J47YM3RVOCXMNQDC453GF5VU.jpg",
+      "publishedAt": "2022-03-28T19:36:00Z",
+      "content": "March 28 (Reuters) - Tesla's (TSLA.O) announcement on Monday that it will seek shareholder approval to increase its share count in order to enable a stock split adds to a recent wave of megacap compa… [+2588 chars]"
+    },
+    {
+      "source": {
+        "id": "reuters",
+        "name": "Reuters"
+      },
+      "author": null,
+      "title": "Israeli startup to test brain-activity gear on space mission to ISS - Reuters.com",
+      "description": "Israel's Brain.Space, a four-year-old startup that studies data on brain activity, is set to put its gear to test on astronauts in space next week during a SpaceX shuttle flight to the International Space Station (ISS).",
+      "url": "https://www.reuters.com/technology/israeli-startup-test-brain-activity-gear-space-mission-iss-2022-03-28/",
+      "urlToImage": "https://www.reuters.com/resizer/cGsWMxzEih3Rc5Wia-6qh6CTTic=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/PUJDPEFMRBOJJFU6OC6ZYEEX7Q.jpg",
+      "publishedAt": "2022-03-28T12:09:00Z",
+      "content": "TEL AVIV, March 28 (Reuters) - Israel's Brain.Space, a four-year-old startup that studies data on brain activity, is set to put its gear to test on astronauts in space next week during a SpaceX shutt… [+2399 chars]"
+    },
+    {
+      "source": {
+        "id": null,
+        "name": "Android Central"
+      },
+      "author": "namerahsaudfatmi@gmail.com (Namerah Saud Fatmi)",
+      "title": "Samsung Galaxy S21 FE vs. Apple iPhone 13: Clash of OS clans",
+      "description": "The iPhone 13 comes with an all-star cast of specifications. On the Android side of things, the Galaxy S21 FE touts value for money, but it may have made one too many compromises to achieve that low price tag. We're here to decide which one's the better phone…",
+      "url": "https://www.androidcentral.com/samsung-galaxy-s21-fe-vs-apple-iphone-13",
+      "urlToImage": "https://cdn.mos.cms.futurecdn.net/z5RB24gK46DaTRcyaZVeM-1200-80.jpg",
+      "publishedAt": "2022-03-28T11:30:48Z",
+      "content": "The Samsung Galaxy S21 FE tries to fill the shoes of the preceding S20 FE, which was a smashing hit for several reasons. Mainly, though, fans were able to get upper-tier specs at an accessible price.… [+10058 chars]"
+    },
+    {
+      "source": {
+        "id": null,
+        "name": "Android Central"
+      },
+      "author": "Patrick Farmer",
+      "title": "How to watch CODA online: a guide to the Best Picture winner",
+      "description": "CODA is the first film with a predominantly deaf cast to win an Academy Award. Here's how you can stream the acclaimed comedy-drama.",
+      "url": "https://www.androidcentral.com/streaming-tv/how-to-watch-coda-online-a-guide-to-the-best-picture-winner",
+      "urlToImage": "https://cdn.mos.cms.futurecdn.net/JdyD5s7VtWZYM8rpfMeDJh-1200-80.jpg",
+      "publishedAt": "2022-03-28T17:18:21Z",
+      "content": "If you missed the Academy Awards last night, you're probably searching for two things: \"Will Smith slap\" and how to watch CODA online. The critically acclaimed coming-of-age drama was pitched against… [+1554 chars]"
+    },
+    {
+      "source": {
+        "id": null,
+        "name": "Android Central"
+      },
+      "author": "Shruti Shekar",
+      "title": "North American telecom companies killed the 'affordable flagship' category to get you to buy expensive phones",
+      "description": "Experts say that the way telecom companies have positioned premium handsets eliminates the need to have an affordable flagship category in North America.",
+      "url": "https://www.androidcentral.com/flagship-android-phones-north-america",
+      "urlToImage": "https://cdn.mos.cms.futurecdn.net/epdC4oi7a6obDrn5kKsQGb-1200-80.jpg",
+      "publishedAt": "2022-03-28T12:00:58Z",
+      "content": "North America’s wine-glass-shaped smartphone market and telecom subsidies are likely the reason why there really isn’t an “affordable flagship” category, experts weigh in. They add that there are a f… [+6688 chars]"
+    },
+    {
+      "source": {
+        "id": null,
+        "name": "Android Central"
+      },
+      "author": "jayar.decenella@gmail.com (Jay Bonggolto)",
+      "title": "HBO Max picks up one of Netflix's best features, but there's a catch",
+      "description": "With a new shuffle play button, HBO Max makes streaming a little easier for indecisive subscribers, but it's only available on desktop for now and is limited to 45 shows.",
+      "url": "https://www.androidcentral.com/streaming-tv/hbo-max-picks-up-one-of-netflixs-best-features-but-theres-a-catch",
+      "urlToImage": "https://cdn.mos.cms.futurecdn.net/HGjp6d5Xh3xZBqRmAGxPTX-1200-80.jpg",
+      "publishedAt": "2022-03-28T11:00:48Z",
+      "content": "<ul><li>HBO Max has introduced a new shuffle play button akin to Netflix and Amazon Prime Video's features.</li><li>It works by randomly playing episodes from any of the 45 shows supported by the fea… [+2801 chars]"
+    },
+    {
+      "source": {
+        "id": null,
+        "name": "Android Central"
+      },
+      "author": "Andrew Myrick",
+      "title": "The Galaxy Tab S8 Ultra could be a laptop replacement if it weren't for the Book Cover Keyboard",
+      "description": "There was a lot of promise for the Tab S8 Ultra and its Book Cover Keyboard, but you're better off looking for a different solution.",
+      "url": "https://www.androidcentral.com/tablets/samsung-galaxy-tab-s8-ultra-book-cover-keyboard-dont-buy",
+      "urlToImage": "https://cdn.mos.cms.futurecdn.net/8zKhR9Rj2Qw3bPNrXN2P3G-1200-80.jpg",
+      "publishedAt": "2022-03-28T13:00:00Z",
+      "content": "If you're one of the madlads that sprung for Samsung's biggest tablet, the Galaxy Tab S8 Ultra, then you likely are the type of person who wants to get some work done at some point. Whether that's ch… [+9045 chars]"
+    }
+  ];
+  
+  
+  
+  
